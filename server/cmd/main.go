604fbf11f2c33951b4cf425b32b5277fb27ef7a1
Joshua Agbeku <mawulidickson@gmail.com>
2021-03-03 11:26:26 +0000

    Made errors more explicit and added update handler

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,18 +9,18 @@ import (
 )
 
 var fs = store.NewFileStore("kv-store", ".fileStore")
+
 //var ms = store.NewInMemoryStore("kv-store")
 
-
-func handleSet(w http.ResponseWriter, r *http.Request){
+func handleSet(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	//queryParams := r.URL.Query() // query-based matching
-	vars := mux.Vars(r) // path-based matching
+	vars := mux.Vars(r)
 	err := fs.Set(vars["key"], vars["val"])
-	//err = fs.Set( queryParams["key"][0],  queryParams["val"][0])
 	if err != nil {
-		http.Error(w,"Could not save record", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Key already exists: %s", vars["key"]), http.StatusNotFound)
+		return
 	}
+	fmt.Fprintf(w, vars["val"]+" OK")
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request) {
@@ -28,11 +28,31 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	value, err := fs.Get(vars["key"])
 	if err != nil {
-		http.Error(w, fmt.Sprintf("No value for the key: %s", vars["key"]), http.StatusNotFound)
+		http.Error(w, fmt.Sprintf("Could not get value for non-exixtent key: %s", vars["key"]), http.StatusNotFound)
 	}
-	_, err = fmt.Fprintf(w, value)
+	fmt.Fprintf(w, value)
+}
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	vars := mux.Vars(r)
+	err := fs.Update(vars["key"], vars["val"])
 	if err != nil {
-		http.Error(w,"Something went wrong", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Could not update value for non-exixtent key: %s", vars["key"]), http.StatusNotFound)
+	}
+	if err == nil {
+		fmt.Fprintf(w, fmt.Sprintf("%s updated to %s", vars["key"], vars["val"]))
+	}
+}
+func handleGetAll(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	//vars := mux.Vars(r)
+	kv, err := fs.GetAll()
+	if err != nil {
+		http.Error(w, "Could not get all records", http.StatusNotFound)
+	}
+	_, err = fmt.Fprintf(w, kv)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 	}
 }
 
@@ -50,14 +70,15 @@ func handlePing(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("PONG"))
 }
 
-func main(){
+func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/v1/kv", handlePing)
-	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}/{val:[0-9]+}", handleSet) // path-based matching
-	//r.HandleFunc("/v1/kv/, handleSet) // query-based matching
-	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}", handleGet).Methods("GET")
-	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}", handleDelete).Methods("POST").Name("deleteRecord")
+	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}/{val:[A-Za-z0-9]+}/set", handleSet).Methods("POST")
+	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}/{val:[A-Za-z0-9]+}/update", handleUpdate).Methods("POST")
+	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}/get", handleGet).Methods("GET")
+	r.HandleFunc("/v1/kv/all", handleGetAll).Methods("GET")
+	r.HandleFunc("/v1/kv/{key:[A-Za-z]+}/delete", handleDelete).Methods("POST").Name("deleteRecord")
 	fmt.Println("kv is starting")
 	fmt.Println("Ready to accept connections")
 	log.Fatal(http.ListenAndServe(":1024", r))
